d8b813ee044f2dfafa5627b71c2e8eb60ce01d1c
JS <kosnsn@gmail.com>
2020-09-11 20:06:13 +0900

    router update

---
+++ src/components/Board.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    Board
+    {{bid}}
+    <hr/>
+    <router-link to="/b/${bid}/c/1">Card 1</router-link>
+    <router-link to="/b/${bid}/c/2">Card 2</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        bid: 0
+      }
+    },
+    created(){
+      this.bid = this.$route.params.bid
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/Login">Login</router-link>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
+
