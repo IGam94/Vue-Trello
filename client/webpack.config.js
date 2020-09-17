10d4d099507f1dc2b2cce8755d71f1699954a957
JS <kosnsn@gmail.com>
2020-09-17 16:20:12 +0900

    port

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -48,7 +48,8 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     noInfo: true,
-    overlay: true
+    overlay: true,
+    port: 7777
   },
   performance: {
     hints: false
